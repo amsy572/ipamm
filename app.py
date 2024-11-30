36fbac2e0a245e482b930509906da4ddaef55119
AMSY_WIMLA <80291638+amsy572@users.noreply.github.com>
2024-11-30 19:28:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def get_answer():
 
 if __name__ == '__main__':
     # Run the Flask app
-    app.run(host="0.0.0.0", port=5000, debug=True)  # Use debug=True for local development only.
+    app.run(host="0.0.0.0", port=5000, debug=True)
